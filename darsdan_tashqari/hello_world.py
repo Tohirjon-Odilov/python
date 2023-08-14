667d3ad9410c0607ba093887e4f9fa7782c921b1
Tohirjon-Odilov <shahanshoh819@gmail.com>
2023-08-14 22:51:11 +0500

    add some files and some changes

---
+++ darsdan_tashqari/hello_world.py
@@ -0,0 +1,51 @@
+""""
+Nexia", "Tico", 'Damas' ko'rganlar qilar havas
+5 ning 4-darajasi 625
+22 ni 4 ga bo'lganda qancha qoldiq 2
+Tomonlari 125 ga teng kvadratning yuzi 15625 ga, perimetri 500 teng
+Diametri 12 ga teng bo'lgan doiraning yuzi 113.04 ga teng
+Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasi 9.219544457292887
+"""
+
+# print('Nexia', "Tico", "Damas", "ko'rganlar qilar havas")
+# str = "Hello world"
+# print(str)
+
+# xabar = "ALisher Navoiy"
+# print(xabar)
+
+# xabar = "Abdulla Qodiriy"
+# print(xabar)
+
+# radius = 5
+# pi = 3.14159
+# aylana_yuzi = pi * radius**2
+# print("Radiusi" , radius, "ga teng aylananing yuzi=", aylana_yuzi)
+
+kocha = "Bog'bon"
+mahalla = "Sog'bon"
+tuman = "Bodomzor"
+viloyat = "Samarqand"
+
+# print(f"{kocha} ko'chasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati")
+
+# kocha = input("Ko'cha nomini kiriting: ")
+# mahalla = input("Mahalla nomini kiriting: ")
+# tuman = input("Tuman nomini kiriting: ")
+# viloyat = input("Viloyatingizni kiriting: ")
+
+# print(f"{kocha} ko'chasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati")
+
+# kocha = input("Ko'cha nomini kiriting: ")
+# mahalla = input("Mahalla nomini kiriting: ")
+# tuman = input("Tuman nomini kiriting: ")
+# viloyat = input("Viloyatingizni kiriting: ")
+
+# print(f"{kocha.title()} ko'chasi,\n{mahalla.title()} mahallasi,\n{tuman.title()} tumani,\n{viloyat.title()} viloyati\n")
+
+manzil = f"{kocha.title()} ko'chasi,\n{mahalla.title()} mahallasi,\n{tuman.title()} tumani,\n{viloyat.title()} viloyati\n"
+print(manzil.upper())
+print(manzil.lower())
+print(manzil.title())
+print(manzil.capitalize())
+
--- main.py
+++ main.py
@@ -1,1 +1,2 @@
 print('salolar nima gaplar')
+
