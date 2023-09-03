37c067c5e11e805613895f763b89f891a242583c
Tohirjon-Odilov <shahanshoh819@gmail.com>
2023-09-03 19:23:11 +0500

    update some files

--- py_314_random_kwargs_args_t10_t13/lesson.py
+++ py_314_random_kwargs_args_t10_t13/lesson.py
@@ -1,2 +1,50 @@
-with("../py_312_file_t1/booking_data.txt", "r") as f:
+# with("../py_312_file_t1/booking_data.txt", "r") as f:
+#     print("slaom")
+# file1 = open("test1.txt", "x")
+# file1.close()
+if 0:
+    with open("test.txt", "r") as f:
+        # print(f.write("salom dunyoda nima gaplar olamdachi"))
+        # print(f.read())
+        # print(f.fileno())
+        # f.flush()
+        print(f.seek(10))
+        print(f.read())
+# f = open("test.txt", "a")
+# f.write("Now the file has one more line!")
+# f.flush()
+# f.write("...and another one!")
+# print(detach(f))
+# file = open("test.txt", "r")
+# print(file.read())
+if 0:
+    f = open("test.txt", "a")
+    f.truncate(20)
+    f.close()
+
+    # open and read the file after the truncate:
+    f = open("test.txt", "r")
     print(f.read())
+if 1:
+    import random
+
+    # print(random.random())
+    # state = random.getstate()
+    # print(random.random())
+    # random.setstate(state)
+    # print(random.random())
+    # print(random.random())
+
+    # print(random.getrandbits(8))
+    # for i in range(100):
+    #     print(random.randrange(3, 10, 5), end=" ")
+    # print(random.randint(3, 9), end=" ")
+    # x = "WELCOME"
+
+    # print(random.choice("tasodif"))
+
+    mylist = ["apple", "banana", "cherry"]
+    # print(random.choices(mylist, weights=[10, 1, 2], k=14))
+    # print(random.sample(mylist, k=12))
+    # for i in range(100):
+    print(random.betavariate(beta=10, alpha=10))
---
+++ py_exam_1_v1_v2/V1.py
@@ -0,0 +1,169 @@
+# PART I
+
+# Question 1.
+# Set haqida to'liqroq ma'lumot bering. Misol keltiring.
+"""
+1. Set o'zida unikal ma'lumotlarni saqlaydi.
+2. Indexlanmagan shu sababli index'ga murojaat qilsak xatolik beradi.
+3. Xotirada tartibsiz joylashadi.
+4. myset = {"salom", "nima" "gaplar"}
+"""
+# myset = {"salom", "nima", "gaplar"}
+# print(myset)
+
+# Question 2.
+# Quyidagi kodni izohlang.
+x = 42
+# print("no") if x > 42 else print("yes") if x == 42 else print("maybe")
+# "yes" chiqadi. If'ning bir qatordagi varianti bo'lib, birinchi shartga to'g'ri kelmaydi.
+# shundan so'ng else ichidagi if sharti ishga tushib "yes" print qiladi.
+
+
+# Question 3.
+# Lambda function'ga misol bo'luvchi bir kod yozib bering.
+# def myfunc(b):
+#     return lambda a: a**b
+#
+#
+# myfunc2 = myfunc(2)
+# print(myfunc2(5))
+
+
+# Question 4.
+# Quyidagi kod natijasini izohlang.
+# def reverser(a)
+# if len(a) > 10 and a[0] = 'a';
+# return a[::-1]
+# elif len(a) < 10 and a[0] == 'b':
+# return a
+# print(reverser(input()))
+
+# Ushbu kodda funksiyaning : tushib qolgan syntax error va indentation error beradi.
+
+
+# Part II
+
+# Problem 1.
+# String tipida foydalanuvchi bir satr kiritadi. Maqsadingiz shu satr ichida so'zlar bosh va oxirgi harflarining o'rnini
+# almashtirib, paydo bo'lgan satrni chop etuvchi funksiya tuzish.
+
+# text = "London is the capital of great Britain"
+# text = text[::-1].split()
+# text.reverse()
+# text = " ".join(text)
+
+# update
+# text = " ".join((map(lambda a: a[::-1], text.split())))
+# print(text)
+
+# Problem
+
+if 0:
+
+    def alpha_number(num):
+        alpha = {
+            0: "nol",
+            1: "bir",
+            2: "ikki",
+            3: "uch",
+            4: "toʻrt",
+            5: "besh",
+            6: "olti",
+            7: "yetti",
+            8: "sakkiz",
+            9: "to'qqiz",
+            10: "oʻn",
+            20: "yigirma",
+            30: "o'ttiz",
+            40: "qirq",
+            50: "ellik",
+        }
+
+        if num in alpha:
+            return alpha[num]
+        elif 10 < num < 60 and num % 10 != 0:
+            tens = num // 10 * 10
+            units = num % 10
+            return alpha[tens] + " " + alpha[units]
+        else:
+            return "Invalid number entered"
+
+    def letter_position(letter):
+        letter = letter.lower()
+        alpha = "abcdefghijklmnopqrstuvwxyz"
+        if letter in alpha:
+            index = alpha.index(letter) + 1
+            return index
+        else:
+            return "Invalid letter entered"
+
+    try:
+        user_input = input("Enter a letter or a number: ")
+        if user_input.isdigit():
+            number = int(user_input)
+            if 0 <= number <= 59:
+                name = alpha_number(number)
+                print(name)
+            else:
+                print("Please enter a number between 0 and 59.")
+        else:
+            letter_index = letter_position(user_input)
+            print(letter_index)
+    except ValueError:
+        print("Please enter a valid number or letter.")
+
+
+# Problem 3.
+# Biror dictionary tipidagi ma'lumotni fayl ichiga dictionary ko'rinishida yozuvchi,
+# va shundan so'ng xuddi shu fayl ichidan
+# o'qib yana dictionary ko'rinishiga keltirib beruvchi dastur tuzing. Misollar quyida keltirilgan.
+if 0:
+    mydict = {
+        "first_name": "John",
+        "last_name": "Doe",
+        "age": 34,
+        "student": False,
+        "worker": True,
+    }
+
+    with open("file.txt", "w") as f:
+        f.write("{")
+        for key, val in mydict.items():
+            if type(val) == str:
+                f.write(f'"{key}": "{val}",')
+            else:
+                f.write(f"{key}: {val},")
+
+        f.write("}")
+if 1:
+    # update
+    mydict = {
+        "first_name": "John",
+        "last_name": "Doe",
+        "age": 34,
+        "student": False,
+        "worker": True,
+    }
+
+    with open("file.txt", "w") as f:
+        f.write("{\n")
+        for key, val in mydict.items():
+            if isinstance(val, str):
+                f.write(f'    "{key}": "{val}",\n')
+            else:
+                f.write(f'    "{key}": {val},\n')
+        f.write("}\n")
+
+if 0:
+    import json
+
+    data = {
+        "first_name": "John",
+        "last_name": "Doe",
+        "age": 34,
+        "student": False,
+        "worker": True,
+    }
+
+    with open("data.json", "w") as file:
+        json.dump(data, file)
