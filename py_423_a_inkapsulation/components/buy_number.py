b798e7ea93bc675f52b909209d45505780e8c76b
Tohirjon-Odilov <shahanshoh819@gmail.com>
2023-09-18 13:29:10 +0500

    shu yergacha xatosiz, admin panel qolgan

--- py_423_a_inkapsulation/components/buy_number.py
+++ py_423_a_inkapsulation/components/buy_number.py
@@ -19,99 +19,60 @@ def choice_number(company):
     elif selected_number == "0":
         exit("Program stopped!")
     else:
-        return ""
-        print("Unexpected number!\nPlease choice only show numbers!")
+        input("Unexpected number!\nPlease choice only show numbers!")
+        return "2"
 
 
 def all_numbers():
     # system("clear")
-    selected_company = select_company()
-    if selected_company == "1":
-        while True:
-            company = Company("Beeline")
-            is_comeback = choice_number(company)
-            if is_comeback == "2" or is_comeback == "":
-                continue
-            else:
-                return is_comeback, company
-    elif selected_company == "2":
-        while True:
-            company = Company("MobiUz")
-            is_comeback = choice_number(company)
-            if is_comeback == "2" or is_comeback == "":
-                continue
-            else:
-                return is_comeback
+    while True:
+        selected_company = select_company()
+        if selected_company == "1":
+            while True:
+                company = Company("Beeline")
+                is_comeback = choice_number(company)
+                if is_comeback == "":
+                    break
+                    continue
+                elif is_comeback == "2":
+                    continue
+                else:
+                    return is_comeback, company
+        elif selected_company == "2":
+            while True:
+                company = Company("MobiUz")
+                is_comeback = choice_number(company)
+                if is_comeback == "2" or is_comeback == "":
+                    continue
+                else:
+                    return is_comeback
 
-        # company = Company("MobiUz")
-        # show_number(company)
-        # selected_number = select_number()
-        # if selected_number == "1":
-        #     return int(input("Enter id: ")) - 1, company
-        # elif selected_number == "2":
-        #     return "2", company
-        # elif selected_number == "0":
-        #     exit("Program stopped!")
-        # else:
-        #     print("Unexpected number!")
-    elif selected_company == "3":
-        while True:
-            company = Company("Ucell")
-            is_comeback = choice_number(company)
-            if is_comeback == "2" or is_comeback == "":
-                continue
-            else:
-                return is_comeback
-        # company = Company("Ucell")
-        # show_number(company)
-        # selected_number = select_number()
-        # if selected_number == "1":
-        #     return int(input("Enter id: ")) - 1, company
-        # elif selected_number == "2":
-        #     return "2", company
-        # elif selected_number == "0":
-        #     exit("Program stopped!")
-        # else:
-        #     print("Unexpected number!")
-    elif selected_company == "4":
-        while True:
-            company = Company("Uzmobile")
-            is_comeback = choice_number(company)
-            if is_comeback == "2" or is_comeback == "":
-                continue
-            else:
-                return is_comeback
-        # company = Company("Uzmobile")
-        # show_number(company)
-        # selected_number = select_number()
-        # if selected_number == "1":
-        #     return int(input("Enter id: ")) - 1, company
-        # elif selected_number == "2":
-        #     return "2", company
-        # elif selected_number == "0":
-        #     exit("Program stopped!")
-        # else:
-        #     print("Unexpected number!")
-    elif selected_company == "5":
-        while True:
-            company = Company("numbers")
-            is_comeback = choice_number(company)
-            if is_comeback == "2" or is_comeback == "":
-                continue
-            else:
-                return is_comeback
-        # company = Company("numbers")
-        # show_number(company)
-        # selected_number = select_number()
-        # if selected_number == "1":
-        #     return int(input("Enter id: ")) - 1, company
-        # elif selected_number == "2":
-        #     return "2", company
-        # elif selected_number == "0":
-        #     exit("Program stopped!")
-        # else:
-        #     print("Unexpected number!")
-    elif selected_company == "0":
-        exit("Program stopped!")
-        # uzmobile = Company("Uzmobile")
-        # print(uzmobile.company())
+        elif selected_company == "3":
+            while True:
+                company = Company("Ucell")
+                is_comeback = choice_number(company)
+                if is_comeback == "2" or is_comeback == "":
+                    continue
+                else:
+                    return is_comeback
+
+        elif selected_company == "4":
+            while True:
+                company = Company("Uzmobile")
+                is_comeback = choice_number(company)
+                if is_comeback == "2" or is_comeback == "":
+                    continue
+                else:
+                    return is_comeback
+
+        elif selected_company == "5":
+            while True:
+                company = Company("numbers")
+                is_comeback = choice_number(company)
+                if is_comeback == "2" or is_comeback == "":
+                    continue
+                else:
+                    return is_comeback
+
+        elif selected_company == "0":
+            exit("Program stopped!")
