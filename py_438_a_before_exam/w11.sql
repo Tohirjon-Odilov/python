8c1cbe422617eaf9c4ccad78eae97dca81fda64f
Tohirjon-Odilov <shahanshoh819@gmail.com>
2023-10-06 12:03:47 +0500

    update files

--- py_438_a_before_exam/w11.sql
+++ py_438_a_before_exam/w11.sql
@@ -7,15 +7,15 @@ CREATE TABLE Company(
     location VARCHAR(50),
     capital INT,
     employees_count INT,
-    establishedAt INT,
+    establishedAt DATE,
     monthly_expenses INT
 );
 INSERT INTO Company
-VALUES ('Amazon', 'Seattle', 200, 500, 2010, 1000),
-    ('Facebook', 'New York', 300, 1000, 2011, 2000),
-    ('Google', 'Tashkent', 500, 1500, 2012, 3000),
-    ('Microsoft', 'Redmond', 200, 2000, 2013, 4000),
-    ('Twitter', 'San Francisco', 500, 2500, 2014, 5000);
+VALUES ('Amazon', 'Seattle', 200, 500, "2010-10-01", 1000),
+    ('Facebook', 'New York', 300, 1000, "2011-10-01", 2000),
+    ('Google', 'Tashkent', 500, 1500, "2012-10-01", 3000),
+    ('Microsoft', 'Redmond', 200, 2000, "2013-10-01", 4000),
+    ('Twitter', 'San Francisco', 500, 2500, "2014-10-01", 5000);
     
 -- 1
 SELECT *
@@ -47,5 +47,6 @@ FROM Company
 GROUP BY location;
 
 -- 6
-SELECT name, ((2023 - establishedAt) * 12) * monthly_expenses AS months_old
-FROM Company;
+SELECT name, ((2023 - establishedAt) * 12) * monthly_expenses AS months_old FROM Company;
+
+SELECT name, ((2023 - YEAR(establishedAt)) * 12) * monthly_expenses AS months_old FROM Company;
