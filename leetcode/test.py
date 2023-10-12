b5bf595979be02da0168d4e38830a26a79640319
Tohirjon-Odilov <shahanshoh819@gmail.com>
2023-10-12 11:03:40 +0500

    896. Monotonic Array

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,4 +1,12 @@
-
+# 896. Monotonic Array
+if 1:
+    class Solution:
+        def isMonotonic(self, nums: list[int]) -> bool:
+            return True if nums == list(sorted(nums)) or list(reversed(sorted(nums))) == nums else False
+        
+    print(Solution().isMonotonic([1,2,2,3]))
+    print(Solution().isMonotonic([6,5,4,4]))
+    print(Solution().isMonotonic([6,3,4,2]))
 
 
 # 941. Valid Mountain Array
