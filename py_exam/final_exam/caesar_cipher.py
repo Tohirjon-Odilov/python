31360a7b956a4c91e7e905ff34dc1d952028e181
Tohirjon-Odilov <shahanshoh819@gmail.com>
2023-10-17 20:58:35 +0500

    update two solution

--- py_exam/final_exam/caesar_cipher.py
+++ py_exam/final_exam/caesar_cipher.py
@@ -1,16 +1,36 @@
+# def caesar_cipher(text, key):
+#     res = str()
+#     text = text.upper()
+#     for i in text:
+#         num = ord(i)
+#         if 65 <= num+key <= 90:
+#             res += chr((num+key))
+#         elif num == 32:
+#             res += " "
+#         else:
+#             res += chr((num+key)%91+65)
+#     return res.lower()
+
+# print(caesar_cipher("hello", 5))
+# print(caesar_cipher("hello world", 1))
+# print(caesar_cipher("xyz", 2))
+
 def caesar_cipher(text, key):
-    # text = text.lower()
-    # shifr = list("abcdefghijklmnopqrstuvwxyz")
-    # res = list()
-    # 
-    # for i in range(len(shifr)):
-        # for j in text:
-            # if shifr[i] == j:
-                # res.append(shifr[i+key])
-
-    return "".join(list(chr(ord(i)+key) for i in text))
-    # return "".join(res)
-
+    retstr = ""
+    lst = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    for i in text:
+        if i == " ":
+            retstr += " "
+            continue
+        
+        i = i.upper()
+        if i in lst:
+            char = lst.index(i)
+            retstr += lst[(char+key)%len(lst)]
+            
+    return retstr.lower()
+    
 print(caesar_cipher("hello", 5))
 print(caesar_cipher("hello world", 1))
 print(caesar_cipher("z", 2))
+    
