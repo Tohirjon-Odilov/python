9e992f006b4351e505a5f9feacc4649ce6328bf1
Tohirjon-Odilov <shahanshoh819@gmail.com>
2023-10-20 20:30:45 +0500

    add exam solution

---
+++ py_exam/final_exam/c_format.py
@@ -0,0 +1,22 @@
+def c_format(text: str) -> str:
+    text = text.split("\n")
+    new_text = str()
+    for _, el in enumerate(text):
+        if el.find("+1") != -1:
+            j = len(el) - len(el.strip())
+            new_text += f"{' ' * j}{el.strip()[0]}++;"
+        else:
+            new_text += el
+        new_text += "\n" 
+    return new_text
+
+print(c_format("""#include<stdio.h>
+int main()
+{
+    int a=5;
+    a=a+1;
+    int b=a*a;
+    b=b+1;
+    printf("a=%d\\tb=%d\\n", a,b);
+    return 0;
+}"""))
---
+++ py_exam/final_exam/donut.py
@@ -0,0 +1,14 @@
+def dublicate_zeros(nums:str) -> list[int]:
+    nums = eval(nums)
+    num = list()
+    for el in nums:
+        if el == 0:
+            num.append(0)
+        num.append(el)
+    
+    return num
+
+# print(dublicate_zeros("[1,0,2,3,0,4,5,0]"))
+# print(dublicate_zeros("[0,0,0,0]"))
+# print(dublicate_zeros("[100, 10, 0, 0, 101, 1000]"))
+print(dublicate_zeros(input("Enter list: ")))
