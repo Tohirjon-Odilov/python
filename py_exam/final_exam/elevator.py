0259c32bd14c49192e54ac94cc0fc3c2a8e6f9af
Tohirjon-Odilov <shahanshoh819@gmail.com>
2023-10-20 21:35:44 +0500

    add elevator

---
+++ py_exam/final_exam/elevator.py
@@ -0,0 +1,32 @@
+def elevator(nums: str) -> str:
+    nums = ",".join(nums.split('\n')).split(',')
+    # myDict = dict()
+    # for i in range(10, -1, -1):
+        # myDict[i] = []
+    myDict = {i: [] for i in range(10, -1, -1)}
+    for i in nums:
+        if i.isdigit():
+            i = int(i)
+            myDict[i].append(i)
+            
+    for key, value in myDict.items():
+        if not value:
+            myDict[key].append("-")
+
+    result = str()
+    for i in myDict.values():
+        result += ",".join(list(map(str, i))) + "\n"
+    return result[:-1]
+
+# print(elevator('''1,4,3,2
+# 1,10,2
+# -
+# 3,6,4,5,6
+# -
+# -
+# 0,0,0
+# -
+# 4
+# 6,5,2
+# -'''))
+print(elevator(input(">>> ")))
