0bd07fc801a1ab35caaa76e98f9f62ea401c6364
Tohirjon-Odilov <shahanshoh819@gmail.com>
2023-10-26 22:31:34 +0500

    add some files

--- py_exam/final_exam/minimal_change.py
+++ py_exam/final_exam/minimal_change.py
@@ -1,7 +1,7 @@
 def minimal_change(num: int) -> int:
     num = str(num)
     new_num = list(map(lambda a: a*len(num), list(num)))
-    res = list()
+    lst = list()
     for i in num:
         nums = list()
         i = int(i)
@@ -9,10 +9,13 @@ def minimal_change(num: int) -> int:
             if i != j:
                 j = int(j)
                 nums.append(abs(i-j))
-        res.append(sum(nums))
+        lst.append(sum(nums))
 
-    id = min(res)
-    return f"{new_num[res.index(id)]} -> {id}"
+    id = min(lst)
+    res = [f"{new_num[i]} -> {id}" for i in range(len(lst)) if id == lst[i]]
+    return "\n".join(res)
 
-print(minimal_change(293))
-print(minimal_change(8471))
+print(minimal_change(int(input(">>> "))))
+
+# print(minimal_change(293))
+# print(minimal_change(8471))
